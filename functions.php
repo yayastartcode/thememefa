112c441669f4b5e76d7ac2d44533114b6785b4cc
yayastartcode <ordertokoviral@gmail.com>
2025-10-22 10:11:51 +0700

    refactor javascript

--- functions.php
+++ functions.php
@@ -153,6 +153,11 @@ function beritanih_scripts() {
 	// Custom utilities loaded after Tailwind output to prevent being overwritten by watch builds
 	wp_enqueue_style( 'beritanih-custom-utilities', get_template_directory_uri() . '/css/custom-utilities.css', array( 'beritanih-style' ), BERITANIH_VERSION );
 	wp_enqueue_script( 'beritanih-script', get_template_directory_uri() . '/js/script.min.js', array(), BERITANIH_VERSION, true );
+	
+	// Enqueue sliders script only on front page
+	if ( is_front_page() ) {
+		wp_enqueue_script( 'beritanih-sliders', get_template_directory_uri() . '/js/sliders.min.js', array(), BERITANIH_VERSION, true );
+	}
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
